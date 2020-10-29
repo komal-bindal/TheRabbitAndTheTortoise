33f0127f6f8006311e18e5b368bd36490471a810
Komal Bindal <komal.bindal_cs19@gla.ac.in>
2020-10-29 17:49:53 +0530

    add run() method in Tortoise.java and a final MILESTONE variable

--- src/thread/tortoise/Tortoise.java
+++ src/thread/tortoise/Tortoise.java
@@ -1,7 +1,15 @@
 package thread.tortoise;
 
 public class Tortoise extends Thread {
+
+    public static final int MILESTONES = 5;
+
     public Tortoise() {
         super("Tortoise");
+    }
+
+    @Override
+    public void run() {
+
     }
 }
